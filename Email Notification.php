56226366e3b92b8b79d718ce155bd887e0307cdd
KitB <kit@vidinterns.com>
2018-05-29 14:06:56 -0300

    Trying other code for notification
    Tried adding a button to clcik whihc would send an email

--- Email Notification.php
+++ Email Notification.php
@@ -1,47 +1,22 @@
+<form action="" method="post">
+    <input type="submit" value="Send details to embassy" />
+    <input type="hidden" name="button_pressed" value="1" />
+</form>
+
 <?php
 
-if(is_page(file:///D:/Visual%20code%20Test/Updated%20Test%20Page%20(Use%20this).html))
+if(isset($_POST['button_pressed']))
 {
+    $to      = 'nobody@example.com';
+    $subject = 'the subject';
+    $message = 'hello';
+    $headers = 'From: webmaster@example.com' . "\r\n" .
+        'Reply-To: webmaster@example.com' . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
 
-$fromName = 'Auto email notification system';
+    mail($to, $subject, $message, $headers);
 
-$subject = 'Confirmed';
-
-/* Mail Address */
-$toAddr = 'kitbolton884@gmail.com'; 
-$bccAddr = 'bccperson@domain.com'; 
-$fromAddr = 'no-reply@domain.com';
-/* End Mail Address */
-
-
-/* Mail Body */
-$msg = '
-<html>
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title></title>
-</head>
-<body>
-my messages about confirmation...
-</body>
-</html>
-';
-
-$msg = wordwrap($msg, 70);
-/* End Mail Body */
-
-
-/* Mail Headers Setup */
-$headers = array();
-$headers[] = "MIME-Version: 1.0";
-$headers[] = "Content-type: text/html; charset=utf-8";
-$headers[] = "From: ".$fromName." <".$fromAddr.">";
-$headers[] = "Bcc: <".$bccAddr.">";
-/* End Mail Headers Setup */
-
-
-mail($toAddr, $subject, $msg, implode("\r\n", $headers));
-
+    echo 'Email Sent.';
 }
 
 ?>
